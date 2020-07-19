3b83ad0459fa6021d61e527713b76c070f7e7f51
snowdigital <60717268+snowdigital@users.noreply.github.com>
2020-07-19 20:08:55 +0300

    Add files via upload

---
+++ src/components/footer.js
@@ -0,0 +1,10 @@
+import { Link } from "gatsby"
+import React from "react"
+
+const Footer = () => (
+  <footer>
+        Сделано в <a target="_blank" href="https://snowdigital.by/">Snowdigital</a>
+  </footer>
+)
+
+export default Footer
